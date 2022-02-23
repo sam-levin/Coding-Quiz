5e3f0d7b62343826bd916a78659df8bcc3d3d336
sam-levin <sam.ky.levin@gmail.com>
2022-02-22 17:49:46 -0800

    adding basic styling and header to HTML

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// coding questions "What is the purpose of an API" "What does API stand for?" "Which of the following is NOT a type of variable" "Which of the following is the correct way to define a class in CSS"
+
+
+
+// for questions in the length questions loop
+// do the question function
+// question function should give a question, and have a response within it
