ef448ff6d9a1c16c01694ea9e4b35cbada08185c
sam-levin <sam.ky.levin@gmail.com>
2022-02-28 22:04:22 -0800

    finishing touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,21 @@
 
 
 
-// for questions in the length questions loop
-// do the question function
-// question function should give a question, and have a response within it
-
-
-// first build a simple one that just has one question
-
 
 var qaEl = document.querySelector("#question-container");
 var questionEl = document.querySelector("#questions");
 var answerbox = document.getElementById("answers");
 var startBtnEl = document.getElementById("starter");
+var highscorebuttonEl = document.getElementById("high-score");
+var highscoreEl = document.querySelector(".highscore-box");
+var containerEl = document.querySelector(".holder")
+var scoreboard = []
+var timelimit = 200;
+var initialtime = timelimit;
+
+
+
+
 
 
 
@@ -21,66 +24,102 @@ var startBtnEl = document.getElementById("starter");
 const questions = [
     {
         title: "question1",
-        question: "What is your name?",
-        answers: ["Sam",
-        "Mark",
-        "Todd",
-        "Bailey"],
-        correctanswer: "Sam"
+        question: "What is the purpose of an API",
+        answers: ["To store all the data of a webpage",
+        "To act as an intermediary between the user and script that allows the user to interact with the webpage",
+        "To act as an independent AI that acts before the user does, therefore optimizing the webpage",
+        "To style the webpage using classes"],
+        correctanswer: "To act as an intermediary between the user and script that allows the user to interact with the webpage"
     },
     {
         title: "question2",
-        question: "What is your age",
-        answers: [20,
-        30,
-        40,
-        22],
-        correctanswer: 22
+        question: "Which of the following is the correct way to define a class with the name 'classname' in CSS",
+        answers: ["#classname",
+        "%classname",
+        ".classname",
+        "(classname)"],
+        correctanswer: ".classname"
     },
     {
         title: "question3",
-        question: "Penis?",
-        answers: ["Sam",
-        "Mark",
-        "Todd",
-        "Bailey"],
-        correctanswer: "Sam"
-    }
+        question: "To store something into the local storage, it must first be turned into a _____",
+        answers: ["String",
+        "Element",
+        "Javascript",
+        "Array"],
+        correctanswer: "String"
+    },
+    {
+        title: "question4",
+        question: "What is JS short for",
+        answers: ["Jello Shots",
+        "Just Screens",
+        "Java Script",
+        "Jam Shingles"],
+        correctanswer: "Java Script"
+    },
+    {
+        title: "question5",
+        question: "Which of the following is a type of loop",
+        answers: ["For",
+        "Else If",
+        "If",
+        "Else"],
+        correctanswer: "For"
+    } 
+    
 
 ];
 
-//startQuiz(i);
 var questionnum = 0;
+
+var countdown = function () {
+    var subtract = function (){
+        if (initialtime > 0 && questionnum < questions.length){
+            initialtime --; 
+            var timerel = document.querySelector(".timerholder"); 
+            var newtimer = document.createElement("h3");
+            timerel.textContent = "Time Remaining: " + initialtime;
+            timerel.appendChild(newtimer); 
+        } else if (questionnum >= questions.length){
+            clearInterval(timer);
+        } else {
+            clearInterval(timer)
+            gethighscore();
+        }
+    }
+    var timer = setInterval(subtract, 1000);
+    
+}
+
 
 
 
 // this should have an input that lets it know which one it is
 var getqanda = function (currentq) {
+    if (initialtime == timelimit) {
+        countdown();}
     startBtnEl.remove();
     let currentquestion = questions[currentq];
     var q = currentquestion.question;
     var answerlist = currentquestion.answers;
             
-    startQuiz(q, answerlist)
-    
-    
-       
-}
+    window.currentQuestionEl = createQuestionEl(q);
+    questionEl.appendChild(window.currentQuestionEl);
 
-var startQuiz = function(q,answerarray) {
-    createQuestionEl(q);
-    createAnswersEl(answerarray);
+    createAnswersEl(answerlist);
     
-    // maybe this is in a for loop, for i in range of question array?
+    
 }
+    
 
-//when start quiz is done, it should return to the top of the for loop... should start quiz have nextquestion built into it?
+
 
 var createQuestionEl = function (q) {
     var newQEl = document.createElement("h1");
     newQEl.innerHTML = q;
     newQEl.className = "question"
-    questionEl.appendChild(newQEl);
+    return newQEl
 }
 
 var createAnswersEl = function (answerarray) {
@@ -97,12 +136,6 @@ var createAnswersEl = function (answerarray) {
 
 
 
-//var nextquestion = function(){
-
-    // we shouldn't need this as it can be in a for loop
-//}
-
-
 var deleteQnA = function() {
     var qtodelete = document.querySelector(".question");
     qtodelete.remove();
@@ -113,9 +146,81 @@ var deleteQnA = function() {
     }
 
 }
+//functions needed 
+// Need an input for initials
+// Need something to sort high scores by number 
+// Change questions to actual coding questions
+
+
+var save = function (initials){
+    var loadscoreboard = function() {
+        var scoreboard = localStorage.getItem("scoreboard");
+        if (!scoreboard) {
+            newscoreboard = [];
+        } else {
+            newscoreboard = JSON.parse(scoreboard);
+        }
+    }
+    var createhstext = function (){
+        var hstext = document.createElement("h3");
+        hstext.textContent = "High Scores:";
+        hstext.className = "highscore-text"
+        highscoreEl.appendChild(hstext);
+    }
+    createhstext();
+    scoreandname = [];
+    scoreandname.push(initials);
+    scoreandname.push(initialtime);
+    loadscoreboard();
+    newscoreboard.push(scoreandname) // this needs to be combined with initials so that it pushes an array of [SL, 185] to the highscore array
+    for (i = 0; i < newscoreboard.length; i++) {
+        var score = newscoreboard[i];
+        var scoreEl = document.createElement("h3");
+        scoreEl.innerHTML = score;
+        highscoreEl.appendChild(scoreEl);
+    }
+    localStorage.setItem("scoreboard",JSON.stringify(newscoreboard))
+
+    
+}
+
+
 
 var gethighscore = function() {
-    console.log("get high score function ran successfully")
+    var timer = document.querySelector(".timerholder")
+    timer.remove();
+    
+    
+    var initialsprompt = document.createElement("h2")
+    initialsprompt.textContent = ("All done! Input your initials here to be added to the leaderboard")
+    containerEl.appendChild(initialsprompt);
+
+    // this creates a form called initials form and adds it to the container el
+    var initialsform = document.createElement("form");
+    containerEl.appendChild(initialsform);
+    
+    var initialsEl = document.createElement("input")
+    initialsEl.setAttribute("type", "text");
+    initialsEl.className = "initials"
+    initialsform.appendChild(initialsEl);
+
+    var initialsbtn = document.createElement("input");
+    initialsbtn.innerHTML = ("Submit")
+    initialsbtn.id = ("submitbtn")
+    initialsbtn.setAttribute("type", "submit");
+    initialsform.appendChild(initialsbtn);
+
+    var initialformhandler = function(event) {
+        event.preventDefault();
+        initialsprompt.remove();
+        var initialsInput = initialsEl.value;
+        save(initialsInput);
+        initialsform.remove();
+    }
+    initialsform.addEventListener("submit", initialformhandler);
+
+
+
 }
 
 var answerQuestion = function(event) {
@@ -124,24 +229,27 @@ var answerQuestion = function(event) {
     let rightanswer = questions[questionnum].correctanswer;
     let answerchosen = chosenbtn.textContent;
     if (rightanswer == answerchosen) {
-        console.log("True!")
         deleteQnA();
         questionnum ++;
         if (questionnum >= questions.length) {
             gethighscore();
+            countdown();
         } else {
             getqanda(questionnum);
         }
     } else {
-        console.log("false :(")
+        initialtime = initialtime - 10;
         deleteQnA();
         questionnum ++;
         if (questionnum >= questions.length) {
             gethighscore();
+            countdown();
         } else {
             getqanda(questionnum);
         }
     }
 }
+
+
 
 startBtnEl.addEventListener("click", function() {getqanda(questionnum)});
